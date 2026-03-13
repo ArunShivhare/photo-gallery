ad754b055aa442d99cd2b54833a264e16c90b954
Arun Shivhare <arunshivhare925@gmail.com>
2026-03-13 21:17:24 +0530

    finishing

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -1,12 +1,8 @@
 function PhotoCard({ photo, favourites, toggleFavourite }) {
-
-  const isFavourite = favourites.some(
-    (fav) => fav.id === photo.id
-  );
+  const isFavourite = favourites.some((fav) => fav.id === photo.id);
 
   return (
     <div className="bg-white shadow rounded-lg overflow-hidden">
-
       <img
         src={photo.download_url}
         alt={photo.author}
@@ -14,22 +10,17 @@ function PhotoCard({ photo, favourites, toggleFavourite }) {
       />
 
       <div className="p-4 flex justify-between items-center">
+        <p className="text-sm font-medium">{photo.author}</p>
 
-        <p className="text-sm font-medium">
-          {photo.author}
-        </p>
-
-        <button
-          onClick={() => toggleFavourite(photo)}
-          className={`text-xl ${
-            isFavourite ? "text-red-500" : "text-gray-400"
-          }`}
-        >
-          ❤️
+        <button onClick={() => toggleFavourite(photo)} className="text-xl">
+          <img
+            width={16}
+            height={16}
+            src={isFavourite ? "/heart.png" : "/like.png"}
+            alt="like"
+          />
         </button>
-
       </div>
-
     </div>
   );
 }
